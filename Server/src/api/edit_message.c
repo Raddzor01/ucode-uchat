651c3bbaa9c21a2029e3ba70bc8f31e3ca39b82b
Raddzor01 <danil.garin.04@gmail.com>
2023-04-08 19:59:36 +0300

    Edit message fix

--- Server/src/api/edit_message.c
+++ Server/src/api/edit_message.c
@@ -2,18 +2,17 @@
 
 void edit_message(cJSON *json, t_client_info *client_info)
 {
-
     int message_id;
     int chat_id;
-    char *new_text;
+    char *new_message;
     char *query;
 
     message_id = cJSON_GetObjectItem(json, "id")->valueint;
     chat_id = cJSON_GetObjectItem(json, "chat_id")->valueint;
-    new_text = cJSON_GetObjectItemCaseSensitive(json, "text")->valuestring;
+    new_message = cJSON_GetObjectItemCaseSensitive(json, "text")->valuestring;
 
-    query = sqlite3_mprintf("UPDATE messages SET text = '%s' WHERE id = '%d' AND user_id = '%d' AND chat_id = '%d'",
-                            new_text, message_id, client_info->user->id, chat_id);
+    query = sqlite3_mprintf("UPDATE messages SET message = '%s' WHERE id = '%d' AND user_id = '%d' AND chat_id = '%d'",
+                            new_message, message_id, client_info->user->id, chat_id);
     db_execute_query(query);
 
     sqlite3_free(query);
